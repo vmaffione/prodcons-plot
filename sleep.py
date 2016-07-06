4077dd027942d4dcb79600421f9bdc14a6b05687
Vincenzo Maffione <v.maffione@gmail.com>
2016-07-06 23:23:19 +0200

    support notify

--- sleep.py
+++ sleep.py
@@ -22,6 +22,10 @@ class ProdConsState:
         self.pkts = 0
         self.prod_sleeps = 0
         self.cons_sleeps = 0
+        self.prod_kicks = 0
+        self.cons_kicks = 0
+        self.cons_active = False
+        self.prod_active = True
 
     def dump(self):
         from matplotlib import pyplot as plt
@@ -31,12 +35,19 @@ class ProdConsState:
         fig = plt.figure()
         ax = plt.axes(xlim=(0, max_time), ylim=(0, 100))
 
+        print(self.prod_events)
         y = 90
         size = 2
         for ev in self.prod_events:
             if ev[1] == 'z':
                 line = plt.Line2D((ev[0], ev[0] + ev[2]), (y + size/2, y + size/2), lw=2.5)
                 plt.gca().add_line(line)
+            elif ev[1] == 'n':
+                poly = plt.Polygon([[ev[0], y], [ev[0], y + size], [ev[0] + ev[2], y]], color = 'y')
+                plt.gca().add_patch(poly)
+            elif ev[1] == 's':
+                poly = plt.Polygon([[ev[0], y], [ev[0] + ev[2], y], [ev[0] + ev[2], y + size]], color = 'b')
+                plt.gca().add_patch(poly)
             else:
                 rectangle = plt.Rectangle((ev[0], y), ev[2], size, fc='r')
                 plt.gca().add_patch(rectangle)
@@ -46,12 +57,28 @@ class ProdConsState:
             if ev[1] == 'z':
                 line = plt.Line2D((ev[0], ev[0] + ev[2]), (y + size/2, y + size/2), lw=2.5)
                 plt.gca().add_line(line)
+            elif ev[1] == 'n':
+                poly = plt.Polygon([[ev[0], y], [ev[0], y + size], [ev[0] + ev[2], y + size]], color = 'y')
+                plt.gca().add_patch(poly)
+            elif ev[1] == 's':
+                poly = plt.Polygon([[ev[0], y + size], [ev[0] + ev[2], y + size], [ev[0] + ev[2], y]], color = 'b')
+                plt.gca().add_patch(poly)
             else:
                 rectangle = plt.Rectangle((ev[0], y), ev[2], size, fc='g')
                 plt.gca().add_patch(rectangle)
 
         plt.show()
 
+    def future_push(self, t, cb):
+        for i in range(len(self.future)):
+            if t < self.future[i][0] or (t == self.future[i][0] and \
+                    (cb == ProdConsState.prod_sleep_back or \
+                     cb == ProdConsState.cons_sleep_back)):
+                self.future.insert(i, (t, cb))
+                return
+        self.future.append((t, cb))
+
+    # Simulation routines for sleep
     def prod_sleep_front(self):
         if self.qlen < self.args.l:
             self.prod_events.append((self.t, self.pkt_prod, self.args.wp))
@@ -81,21 +108,58 @@ class ProdConsState:
         self.future_push(self.t, ProdConsState.cons_sleep_front)
         self.pkts += 1
 
-    def future_push(self, t, cb):
-        for i in range(len(self.future)):
-            if t < self.future[i][0] or (t == self.future[i][0] and \
-                    (cb == ProdConsState.prod_sleep_back or \
-                     cb == ProdConsState.cons_sleep_back)):
-                self.future.insert(i, (t, cb))
-                return
-        self.future.append((t, cb))
+    # Simulation routines for notifications
+    def prod_ntfy_front(self):
+        if self.qlen < self.args.l:
+            self.prod_events.append((self.t, self.pkt_prod, self.args.wp))
+            self.future_push(self.t + self.args.wp, ProdConsState.prod_ntfy_back)
+        else:
+            self.prod_active = False
+
+    def prod_ntfy_back(self):
+        self.qlen += 1
+        self.pkt_prod += 1
+        t_next = self.t
+        if not self.cons_active:
+            self.cons_active = True
+            self.prod_events.append((self.t, 'n', self.args.np))
+            self.cons_events.append((self.t + self.args.np, 's', self.args.sc))
+            self.future_push(self.t + self.args.np + self.args.sc, ProdConsState.cons_ntfy_front)
+            self.prod_kicks += 1
+            t_next += self.args.np
+        self.future_push(t_next, ProdConsState.prod_ntfy_front)
+
+    def cons_ntfy_front(self):
+        if self.qlen > 0:
+            self.cons_events.append((self.t, self.pkt_cons, self.args.wc))
+            self.future_push(self.t + self.args.wc, ProdConsState.cons_ntfy_back)
+        else:
+            self.cons_active = False
+
+    def cons_ntfy_back(self):
+        self.qlen -= 1
+        self.pkt_cons += 1
+        t_next = self.t
+        self.pkts += 1
+        if not self.prod_active:
+            self.prod_active = True
+            self.cons_events.append((self.t, 'n', self.args.nc))
+            self.prod_events.append((self.t + self.args.nc, 's', self.args.sp))
+            self.future_push(self.t + self.args.nc + self.args.sp, ProdConsState.prod_ntfy_front)
+            self.cons_kicks += 1
+            t_next += self.args.nc
+        self.future_push(t_next, ProdConsState.cons_ntfy_front)
 
 
 def simulate(args):
     pcs = ProdConsState(args)
-    pcs.cons_events.append((0, 'z', args.cons_offset))
-    pcs.future_push(0, ProdConsState.prod_sleep_front)
-    pcs.future_push(args.cons_offset, ProdConsState.cons_sleep_front)
+    if args.algorithm == 'sleep':
+        pcs.cons_events.append((0, 'z', args.cons_offset))
+        pcs.future_push(0, ProdConsState.prod_sleep_front)
+        pcs.future_push(args.cons_offset, ProdConsState.cons_sleep_front)
+    elif args.algorithm == 'notify':
+        pcs.future_push(0, ProdConsState.prod_ntfy_front)
+        pcs.future_push(args.cons_offset, ProdConsState.cons_ntfy_front)
 
     cnt = 0
     while pcs.t <= args.time_max:
@@ -174,14 +238,23 @@ argparser.add_argument('--wp', help = "Wp", type = float, default = 2.0)
 argparser.add_argument('--wc', help = "Wc", type = float, default = 1.0)
 argparser.add_argument('--yp', help = "Yp", type = float, default = 3.0)
 argparser.add_argument('--yc', help = "Yc", type = float, default = 5.0)
+argparser.add_argument('--nc', help = "Nc", type = float, default = 3.0)
+argparser.add_argument('--np', help = "Np", type = float, default = 4.5)
+argparser.add_argument('--sc', help = "Sc", type = float, default = 2.1)
+argparser.add_argument('--sp', help = "Sp", type = float, default = 7.1)
 argparser.add_argument('-l', help = "Queue length", type = int, default = 3)
 argparser.add_argument('--cons-offset', help = "Consumer start delay", type = float, default = 0.0)
 argparser.add_argument('-q', '--quiet', help = "Compute only stats", action='store_true')
+
+argparser.add_argument('-a', '--algorithm', help = "Algorithm",
+                       choices=['sleep', 'notify'], default = 'sleep')
+
 argparser.add_argument('--depends', help = "Dependency on", choices=['yp', 'yc'])
 argparser.add_argument('--ymax', help = "max Yc or Yp when depends is specified",
                        type = int, default = 100)
 argparser.add_argument('--points', help = "number of Yp or Yc points to test when depends is specified",
                        type = int, default = 150)
+
 args = argparser.parse_args()
 
 
